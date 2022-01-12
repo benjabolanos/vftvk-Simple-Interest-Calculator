7283d97cf402aeba1e97e3338c8b785819081be2
benjabolanos <bbolanos@uabc.edu.mx>
2022-01-12 08:18:29 +0000

    Final commit

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    if(checkPrincipal()){
+        var principal = document.getElementById("principal").value;
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate /100;
+        var year = new Date().getFullYear()+parseInt(years);
+        var result =  document.getElementById("result")
+        result.innerHTML="If you deposit "+(principal)
+        +",<br>at an interest rate of "+(rate)+"%<br>You will receive an amount of "
+        +(interest)+",<br>in the year "+(year)+"<br>";
+    } else {
+        alert("Enter a positive number.")
+    }
+}
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
+
+function checkPrincipal(){
+    let principal = document.getElementById("principal").value;
+    if(principal > 0){
+        return true;
+    }
+    return false;
 }
         
