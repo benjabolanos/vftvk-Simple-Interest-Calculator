fb2ee06a5e940fe31a02b7de6cfc8ad7b028d8a8
benjabolanos <bbolanos@uabc.edu.mx>
2022-01-12 09:19:02 +0000

    Web App finished

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ function compute()
         var interest = principal * years * rate /100;
         var year = new Date().getFullYear()+parseInt(years);
         var result =  document.getElementById("result")
-        result.innerHTML="If you deposit "+(principal)
-        +",<br>at an interest rate of "+(rate)+"%<br>You will receive an amount of "
-        +(interest)+",<br>in the year "+(year)+"<br>";
+        result.innerHTML="If you deposit <mark>"+principal
+        +"</mark>,<br>at an interest rate of <mark>"+rate
+        +"%</mark><br>You will receive an amount of <mark>"
+        +interest+"</mark>,<br>in the year <mark>"+year+"</mark><br>";
     } else {
         alert("Enter a positive number.")
     }
@@ -27,5 +28,4 @@ function checkPrincipal(){
         return true;
     }
     return false;
-}
-        
+}       
